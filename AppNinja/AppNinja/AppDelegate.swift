61612070ff5c9b7f1482edce80d45c3a17984c6a
Yuan Zhou <yuan.zhou@eumlab.com>
2015-05-10 10:01:39 +0200

    init project

---
+++ AppNinja/AppNinja/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AppNinja
+//
+//  Created by YuanZhou on 5/10/15.
+//  Copyright (c) 2015 EUMLab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AppNinja/AppNinja/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppNinja
+//
+//  Created by YuanZhou on 5/10/15.
+//  Copyright (c) 2015 EUMLab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
